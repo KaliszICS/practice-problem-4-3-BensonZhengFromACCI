ad056967f7403c4edf83f3d7296ef2ece6a34a79
BensonZhengFromACCI <343804126@tdsb.ca>
2026-04-15 15:24:23 +0000

    dasdasdafdfasd

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,18 +1,50 @@
+import java.util.Scanner;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+q3();
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+    Scanner input = new Scanner(System.in);
+    int num;
+    do {
+        System.out.print("Input a positive integer: ");
+        num = input.nextInt();
+    } 
+	while (num < 0);
+    	System.out.println(num * 2);
+	input.close();
 	}
 
+
 	public static void q2() {
-		//Write question 2 code here
+    Scanner input = new Scanner(System.in);
+    String pass;
+    String passConfirm;
+
+    do {
+        System.out.print("Input a password: ");
+        pass = input.nextLine();
+        System.out.print("Confirm the password: ");
+        passConfirm = input.nextLine();
+    } 
+	while (!pass.equals(passConfirm));
+		System.out.println("success.");
 	}
 
 	public static void q3() {
-		//Write question 3 code here
-	}
+		Scanner input = new Scanner(System.in);
+    	int num;
+		int multiple;
+		do {
+		System.out.print("Input an integer: ");
+		num = input.nextInt();
+		System.out.print("Input an integer that is a multiple of the first integer: ");
+		multiple = input.nextInt();
+		}
+		while (multiple % num != 0);
+			System.out.println("success.");
+	}	
 }
+
